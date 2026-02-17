1edb2ba3bbf99a52e0102dd3cad5ad990ea702d9
Johan Rios <jsrg26@gmail.com>
2026-02-17 12:09:30 -0500

    module 'chess' created for better organization in the main, also adding a decode chess notation function for the square positions

---
+++ chess/src/chess.rs
@@ -0,0 +1,132 @@
+const RESET_COLOR: &str = "\x1b[0m";
+
+const BACKGROUND_COLOR: [&str;2] = [
+    "\x1b[48;2;151;147;204m", //light
+    "\x1b[48;2;94;101;191m", //dark
+];
+
+const PIECE_COLOR: [&str; 3] = [
+    "\x1b[38;2;255;255;255m", //white
+    "\x1b[38;2;0;0;0m", //black
+    "", //no color
+];
+
+
+const PIECE_GRAPH: [char; 7] = [
+    '\u{265A}', //king
+    '\u{265B}', //queen
+    '\u{265C}', //rook
+    '\u{265D}', //bishop
+    '\u{265E}', //knight
+    '\u{265F}', //pawn
+    ' ', //blank
+];
+
+pub fn decode_notation(s: &String) -> Option<usize> {
+    if s.trim().len() == 2 {
+        let j: usize = match s.trim()
+                                .chars()
+                                .next()
+                                .unwrap() {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            'd' => 3,
+            'e' => 4,
+            'f' => 5,
+            'g' => 6,
+            'h' => 7,
+             _  => 8,
+        }; 
+        let i: usize = match s.trim()
+                                .chars()
+                                .nth(1)
+                                .unwrap()
+                                .to_digit(10) {
+            Some(num) => num as usize,
+            None => 8, 
+        };
+        if i < 8 && j < 8{
+            Some(j + i * 8)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Board {
+    id: [u8; 64],
+    color: [u8; 64],
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board { 
+            id: [
+                2, 4, 3, 1, 0, 3, 4, 2,
+                5, 5, 5, 5, 5, 5, 5, 5,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                5, 5, 5, 5, 5, 5, 5, 5,
+                2, 4, 3, 1, 0, 3, 4, 2,
+            ],
+            color: [
+                1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+            ],
+        }
+    }
+
+    pub fn draw(&self) {
+        print!("\n");
+        for n in 0..64 {
+            if(n % 8) == 0 {
+                print!("{:^2}", 8 - n / 8);
+            }
+            let index:usize = (n + n / 8) % 2;
+            print!(
+                "{}{}{:^2}{}",
+                BACKGROUND_COLOR[index],
+                PIECE_COLOR[self.color[n] as usize],
+                PIECE_GRAPH[self.id[n] as usize],
+                RESET_COLOR,
+            );
+            if (n + 1) % 8 == 0 {
+                print!("\n");
+            }        
+        }
+        print!("  ");
+        for n in 'a'..'i' {
+            print!("{:^2}", n);
+        }
+        print!("\n");
+    }
+
+    pub fn move_piece(
+            &mut self, start: &usize, end: &usize
+        ) {
+            if self.id[*start] == 6 {
+                println!("the starting tile is empty");
+                return;
+            }
+            if self.id[*end] != 6 {
+                println!("the finishing tile is not empty");
+                return;
+            }
+            self.id[*end] = self.id[*start];
+            self.color[*end] = self.color[*start];
+            self.id[*start] = 6;
+            self.color[*start] = 2;
+    }
+}
--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,167 +1,26 @@
 use std::io;
+use crate::chess::Board;
 
-const RESET_COLOR: &str = "\x1b[0m";
-
-const BACKGROUND_COLOR: [&str;2] = [
-    "\x1b[48;2;151;147;204m", //light
-    "\x1b[48;2;94;101;191m", //dark
-];
-
-const PIECE_COLOR: [&str; 3] = [
-    "\x1b[38;2;255;255;255m", //white
-    "\x1b[38;2;0;0;0m", //black
-    "", //no color
-];
-
-
-const PIECE_GRAPH: [char; 7] = [
-    '\u{265A}', //king
-    '\u{265B}', //queen
-    '\u{265C}', //rook
-    '\u{265D}', //bishop
-    '\u{265E}', //knight
-    '\u{265F}', //pawn
-    ' ', //blank
-];
-
-#[derive(Clone, Copy)]
-struct Tile {
-    id: u8,
-    color: u8,
-}
-
-impl Tile {
-    pub const fn new(
-            id: u8, color: u8,
-        ) -> Tile {
-        Tile { 
-            id: id,
-            color: color,
-        }
-    }
-} 
-
-const INITIAL_CONFIG: [Tile; 64] = [
-    Tile::new(2, 1), Tile::new(4, 1),
-    Tile::new(3, 1), Tile::new(1, 1),
-    Tile::new(0, 1), Tile::new(3, 1),
-    Tile::new(4, 1), Tile::new(2, 1),
-
-    Tile::new(5, 1), Tile::new(5, 1),
-    Tile::new(5, 1), Tile::new(5, 1),
-    Tile::new(5, 1), Tile::new(5, 1),
-    Tile::new(5, 1), Tile::new(5, 1),
-
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-    Tile::new(6, 2), Tile::new(6, 2),
-
-    Tile::new(5, 0), Tile::new(5, 0),
-    Tile::new(5, 0), Tile::new(5, 0),
-    Tile::new(5, 0), Tile::new(5, 0),
-    Tile::new(5, 0), Tile::new(5, 0),
-
-    Tile::new(2, 0), Tile::new(4, 0),
-    Tile::new(3, 0), Tile::new(0, 0),
-    Tile::new(0, 0), Tile::new(3, 0),
-    Tile::new(4, 0), Tile::new(2, 0),
-];
-
-fn move_piece(
-        board: &mut [Tile; 64],
-        indx_start: u8,
-        indx_finish: u8
-    ) {
-    let start: usize = indx_start as usize;
-    let finish: usize = indx_finish as usize;
-    if board[start].id == 6 {
-        println!("the starting tile is empty");
-        return;
-    }
-    if board[finish].id != 6 {
-        println!("the finishing tile is not empty");
-        return;
-    }
-    board[finish] = board[start];
-    board[start].id = 6;
-    board[start].color = 2;
-}
+pub mod chess;
 
 fn main() {
-    let mut board: [Tile; 64] = INITIAL_CONFIG;
-    
-    for n in 0..64 {
-        if(n % 8) == 0 {
-            print!("{:^2}", 8 - n / 8);
-        }
-        let index:usize = (n + n / 8) % 2;
-        print!(
-            "{}{}{:^2}{}",
-            BACKGROUND_COLOR[index],
-            PIECE_COLOR[board[n].color as usize],
-            PIECE_GRAPH[board[n].id as usize],
-            RESET_COLOR,
-        );
-        if (n + 1) % 8 == 0 {
-            print!("\n");
-        }        
-    }
+    let board: Board = Board::new();
+    board.draw();
 
-    print!("  ");
-    for n in 'a'..'i' {
-        print!("{:^2}", n);
-    }
+    loop {
+        println!("type the square of the piece you want to move");
+        let mut buf: String = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Failed to read the line");
 
-    println!("\nType the piece to move: ");
-    let mut buffer:String = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("cannot read the line");
-    let start_poss: u8 = buffer.trim().parse()
-                                .expect("type a number!");
-
-    println!("\nType the destination tile: ");
-    buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("cannot read the line");
-    let end_poss: u8 = buffer.trim().parse()
-                                .expect("type a number!");
-
-    move_piece(&mut board,start_poss, end_poss);
-
-    for n in 0..64 {
-        if(n % 8) == 0 {
-            print!("{:^2}", 8 - n / 8);
-        }
-        let index:usize = (n + n / 8) % 2;
-        print!(
-            "{}{}{:^2}{}",
-            BACKGROUND_COLOR[index],
-            PIECE_COLOR[board[n].color as usize],
-            PIECE_GRAPH[board[n].id as usize],
-            RESET_COLOR,
-        );
-        if (n + 1) % 8 == 0 {
-            print!("\n");
-        }        
-    }
-
-    print!("  ");
-    for n in 'a'..'i' {
-        print!("{:^2}", n);
+        let index:usize = match chess::decode_notation(&buf) {
+            Some(num) => num,
+            None => {
+                println!("Wrong notation, try agian");
+                continue;
+            },
+        };
+        println!("{}", index);   
     }
 }
