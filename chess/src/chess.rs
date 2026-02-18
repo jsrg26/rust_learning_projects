e1c009330c5b8e4cff18ee4c1a56d7c172043b32
Johan Rios <jsrg26@gmail.com>
2026-02-18 14:18:23 -0500

    adding movement calculation and change draw to reflect movement and capture, also change move to only work on calculated squares

--- chess/src/chess.rs
+++ chess/src/chess.rs
@@ -1,14 +1,22 @@
+type Movement = ([i8;2], u8, i8);
+
 const RESET_COLOR: &str = "\x1b[0m";
 
-const BACKGROUND_COLOR: [&str;2] = [
+const BACKGROUND_COLOR: [&str;8] = [
     "\x1b[48;2;151;147;204m", //light
-    "\x1b[48;2;94;101;191m", //dark
+    "\x1b[48;2;94;101;191m" , //dark
+    "\x1b[48;2;182;173;142m", //light_selected
+    "\x1b[48;2;143;141;133m", //dark_selected
+    "\x1b[48;2;129;175;139m", //light_movement
+    "\x1b[48;2;94;147;131m" , //dark_movement
+    "\x1b[48;2;181;122;176m", //light_capture
+    "\x1b[48;2;147;94;168m" , //dark_capture
 ];
 
 const PIECE_COLOR: [&str; 3] = [
     "\x1b[38;2;255;255;255m", //white
-    "\x1b[38;2;0;0;0m", //black
-    "", //no color
+    "\x1b[38;2;0;0;0m",       //black
+    "",                       //no color
 ];
 
 
@@ -19,10 +27,26 @@ const PIECE_GRAPH: [char; 7] = [
     '\u{265D}', //bishop
     '\u{265E}', //knight
     '\u{265F}', //pawn
-    ' ', //blank
+    ' ',        //blank
 ];
 
-pub fn decode_notation(s: &String) -> Option<usize> {
+const  PIECE_MOVEMENT: [Movement; 5] = [
+    ([1, 0], 8, 1),
+    ([1, 0], 8, 7),
+    ([1, 0], 4, 7),
+    ([1, 1], 4, 7),
+    ([2, 1], 8, 1),
+];
+
+fn sign(num: i8) -> i8 {
+    match num {
+        num if num > 0 => 1,
+        num if num < 0 => -1,
+        _ => 0,
+    }
+}
+
+pub fn decode_notation(s: &String) -> Result<usize, &'static str> {
     if s.trim().len() == 2 {
         let j: usize = match s.trim()
                                 .chars()
@@ -36,7 +60,7 @@ pub fn decode_notation(s: &String) -> Option<usize> {
             'f' => 5,
             'g' => 6,
             'h' => 7,
-             _  => 8,
+             _  => return Err("Wrong letter, try again"),
         }; 
         let i: usize = match s.trim()
                                 .chars()
@@ -44,25 +68,32 @@ pub fn decode_notation(s: &String) -> Option<usize> {
                                 .unwrap()
                                 .to_digit(10) {
             Some(num) => num as usize,
-            None => 8, 
+            None => return Err("Wrong number, try again"), 
         };
-        if i < 8 && j < 8{
-            Some(j + (8 - i) * 8)
+        if i < 9 && i > 0 {
+            Ok(j + (8 - i) * 8)
         } else {
-            None
+            return Err("Wrong number, try again");
         }
     } else {
-        None
+        return Err("Wrong notation, try again");
     }
 }
-
-#[derive(Clone, Copy)]
 pub struct Board {
     id: [u8; 64],
     color: [u8; 64],
+    movement: Vec<u8>,
+    capture: Vec<u8>,
+    pawn_moved: [u8;2],
+    turn: u8,
+    selected: u8,
 }
 
 impl Board {
+    pub fn change_turn(&mut self) {
+        self.turn = (self.turn + 1) % 2;
+    }
+
     pub fn new() -> Board {
         Board { 
             id: [
@@ -85,6 +116,11 @@ impl Board {
                 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0,
             ],
+            movement: Vec::with_capacity(27),
+            capture: Vec::with_capacity(8),
+            pawn_moved: [0; 2],
+            turn: 0,
+            selected: 64,
         }
     }
 
@@ -94,7 +130,16 @@ impl Board {
             if(n % 8) == 0 {
                 print!("{:^2}", 8 - n / 8);
             }
-            let index:usize = (n + n / 8) % 2;
+            let mut index:usize = (n + n / 8) % 2;
+            if self.selected == n as u8{
+                index += 2;
+            }
+            if self.movement.contains(&(n as u8)) {
+                index += 4;
+            }
+            if self.capture.contains(&(n as u8)) {
+                index += 6;
+            }
             print!(
                 "{}{}{:^2}{}",
                 BACKGROUND_COLOR[index],
@@ -113,20 +158,108 @@ impl Board {
         print!("\n");
     }
 
+    pub fn check_start(
+            &self, indx: &usize
+        ) -> Option<&'static str> {
+        if self.id[*indx] == 6 {
+            return Some("the square is empty");
+        } else if self.color[*indx] != self.turn {
+            return Some("the square contains an opponent's piece");
+        }
+        return None;
+    }
+
+    pub fn check_end(
+            &mut self, indx: &usize
+        ) -> Result<u8, &'static str> {
+        if self.movement.contains(&(*indx as u8)) ||
+           self.capture.contains(&(*indx as u8)){
+            self.movement.clear();
+            self.capture.clear();
+            self.selected = 64;
+            return Ok(self.id[*indx]);
+        }
+        Err("Invalid movement for your piece")
+    }
+
+    pub fn eval_reach(
+            &mut self, indx: &usize
+        ) -> Option<&'static str> {    
+        let mut pos: [i8; 2];        
+        let mut new_indx: i8;
+        if self.id[*indx] == 5 {
+            let val: i8 = if self.color[*indx] == 0 {
+                    -1
+                } else {
+                    1
+                };
+            pos = [
+                    (*indx as i8)/8 + val,
+                    (*indx as i8)% 8
+                ];
+            new_indx = pos[1] + pos[0] * 8;
+            if self.id[new_indx as usize] == 6 {
+                self.movement.push(new_indx as u8);
+            } else {
+                return Some("The piece cannot move");
+            }
+            if (self.pawn_moved[self.turn as usize] >> pos[1]) 
+                & 1 == 0 {
+                self.pawn_moved[self.turn as usize] |= 1 << pos[1];
+                new_indx += val * 8;
+                if self.id[new_indx as usize] == 6 {
+                    self.movement.push(new_indx as u8);
+                }
+            } 
+            self.selected = *indx as u8;
+            return None;
+        }
+        let mut mov: Movement = PIECE_MOVEMENT[self.id[*indx] as usize];
+        for rot in 0..mov.1 {  
+            if rot == 4 {
+                mov.0 = [
+                    mov.0[0] - sign(mov.0[1]),
+                    mov.0[1] + sign(mov.0[0])
+                ];
+            }              
+            for reach in 1..(mov.2 + 1) {
+                pos = [
+                    (*indx as i8)/8 + reach * mov.0[0],
+                    (*indx as i8)% 8 + reach * mov.0[1]
+                ];
+                if (pos[0] > -1 && pos[0] < 8) &&
+                    (pos[1] > -1 && pos[1] < 8) {
+                    new_indx = pos[1] + pos[0] * 8;
+                    if self.id[new_indx as usize] == 6 {
+                        self.movement.push(new_indx as u8);
+                    } else {
+                        if  self.color[new_indx as usize] ==
+                            self.turn {
+                            break;
+                        } else {
+                            self.capture.push(new_indx as u8);
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }                
+            mov.0 = [-mov.0[1], mov.0[0]];
+        }
+        if self.movement.len() != 0 || self.capture.len() != 0{
+            self.selected = *indx as u8;
+            return None;
+        }else {
+            return Some("The piece cannot move");
+        }
+    }
+
     pub fn move_piece(
             &mut self, start: &usize, end: &usize
         ) {
-            if self.id[*start] == 6 {
-                println!("the starting tile is empty");
-                return;
-            }
-            if self.id[*end] != 6 {
-                println!("the finishing tile is not empty");
-                return;
-            }
-            self.id[*end] = self.id[*start];
-            self.color[*end] = self.color[*start];
-            self.id[*start] = 6;
-            self.color[*start] = 2;
+        self.id[*end] = self.id[*start];
+        self.color[*end] = self.color[*start];
+        self.id[*start] = 6;
+        self.color[*start] = 2;
     }
 }
--- chess/src/main.rs
+++ chess/src/main.rs
@@ -4,36 +4,76 @@ use crate::chess::Board;
 pub mod chess;
 
 fn main() {
-    let mut board: Board = Board::new();
-    board.draw();
-
-    
+    let mut board: Board = Board::new();    
     let mut buf: String = String::new();
     let mut start_indx:usize = 0;
     let mut end_indx:usize = 0;
+    let mut err_msg: &str = "";
 
-    read_index(&mut buf, &mut start_indx);
-    read_index(&mut buf, &mut end_indx);
-
-    board.move_piece(&start_indx, &end_indx);
-    board.draw();
+    loop {
+        loop {   
+            print!("\x1bc"); 
+            board.draw();
+            println!("\x1b[38;2;255;0;0m{}\x1b[0m", err_msg);
+            println!("type the square of the piece you want to move or 'q' to close:");        
+            if read_index(&mut buf, &mut start_indx) {
+                print!("\x1bc"); 
+                return;
+            }
+            err_msg = match board.check_start(&start_indx) {
+                None => match board.eval_reach(&start_indx) {
+                    None  => break,
+                    Some(msg) => msg, 
+                },
+                Some(msg) => msg,
+            }
+        }
+        err_msg = "";
+        loop {
+            print!("\x1bc"); 
+            board.draw();
+            println!("\x1b[38;2;255;0;0m{}\x1b[0m", err_msg);
+            println!("type the square you want to go to or 'q' to close:");
+            if read_index(&mut buf, &mut end_indx){
+                print!("\x1bc"); 
+                return;
+            }
+            err_msg = match board.check_end(&end_indx) {
+                Ok(indx) => {
+                    println!("\x1bc");
+                    board.move_piece(&start_indx, &end_indx);
+                    board.draw();
+                    if indx != 6 {
+                        println!("you capture a piece!!");
+                    }
+                    break;
+                },
+                Err(msg) => msg,
+            }
+        }
+        err_msg = "";
+        board.change_turn();
+    }
 }
 
-fn read_index(buf: &mut String, indx: &mut usize) {
+fn read_index(buf: &mut String, indx: &mut usize) -> bool{
     loop {
         *buf = String::new();
         io::stdin()
             .read_line(buf)
             .expect("Failed to read the line");
-
+        if buf.trim() == "q" {
+            return true;
+        }
         *indx = match chess::decode_notation(&buf) {
-            Some(num) => num,
-            None => {
-                println!("Wrong notation, try again");
+            Ok(num) => num,
+            Err(msg) => {
+                println!("{}", msg);
                 *buf = String::new();
                 continue;
             },
         };
         break;
     }
+    false
 }
